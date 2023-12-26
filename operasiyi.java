4938975829806cdc618d2e21388ee6899dff55d5
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-26 15:24:48 +0400

    Update operasiyi.java

--- operasiyi.java
+++ operasiyi.java
@@ -1,33 +1,30 @@
 import java.util.Scanner;
- 
+
 public class operasiyi {
- 
-    public static void main(String[] args) {
-        int x=56;
-        int y=12;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Vvedite nomer operasiyi:");
-        System.out.println("1.Slojeniye");
-        System.out.println("2.Vichitaniye");
-        System.out.println("3.Umnojeniye");
-        int a = in.nextInt();
-        
-        
-              
-        switch (a)
-        {
-            case 1:
-                System.out.println(x+y);
-                break;
-            case 2:
-                System.out.println(x-y);
-                break;
-            case 3:
-                System.out.println(x*y);
-                break;
-            default:
-                System.out.println("Neizvestnoye chislo");
-                break;
-        }
-        }
+
+  public static void main(String[] args) {
+    int x = 56;
+    int y = 12;
+    Scanner in = new Scanner(System.in);
+    System.out.println("Vvedite nomer operasiyi:");
+    System.out.println("1.Slojeniye");
+    System.out.println("2.Vichitaniye");
+    System.out.println("3.Umnojeniye");
+    int a = in.nextInt();
+
+    switch (a) {
+    case 1:
+      System.out.println(x + y);
+      break;
+    case 2:
+      System.out.println(x - y);
+      break;
+    case 3:
+      System.out.println(x * y);
+      break;
+    default:
+      System.out.println("Neizvestnoye chislo");
+      break;
+    }
+  }
 }
